1626bfeede19e900500ccf1a464481103bffea81
Moshe Kolodny <moshe@moshe-linux.(none)>
2013-07-30 14:46:11 -0400

    moved to jquery namespace

---
+++ jquery.overlay.js
@@ -0,0 +1,58 @@
+// overlay
+
+// Copyright 2013 Moshe Kolodny
+// Released under the MIT license
+
+$.zerolay = function(options) {
+	var $overlay;
+	
+	options = $.extend({
+		speed: 500,
+		html: ''
+	}, options);
+	
+	$overlay = $('<div>')
+		.css({
+			position: 'fixed',
+			top: 0,
+			bottom: 0,
+			left: 0,
+			right: 0,
+			zIndex: 1041,
+			backgroundColor: options.backgroundColor || 'rgba(0, 0, 0, 0.8)'
+		})
+		.hide()
+		.appendTo(document.body)
+		.fadeIn(options.speed);
+	
+	if (options.html) {
+		$overlay
+			.append(
+				$('<div>').css({
+					display: 'table',
+					width: '100%',
+					height: '100%'
+				}).append(
+					$('<div>').css({
+						display: 'table-cell',
+						verticalAlign: 'middle',
+						textAlign: 'center',
+					}).append(
+						$('<div>')
+							.css({
+								display: 'inline-block',
+								backgroundColor: '#FFF'
+							})
+							.addClass('overlay-div')
+							.html(options.html)
+					)
+				)
+			);
+	}
+	
+	return function(speed) {
+		$overlay.stop().fadeOut(arguments.length ? speed : options.speed, function() {
+			$overlay.remove();
+		});
+	}
+}
